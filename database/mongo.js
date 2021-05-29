239b1ad506d177bf7cec08e6a22f58f59a0527cc
Jorge Emanoel <eu.jorge.emanoell@gmail.com>
2021-05-29 07:15:40 -0300

    add DB_PORT to db connection string

--- database/mongo.js
+++ database/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports = function () {
-  mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}?authSource=admin`, {
+  mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     user: process.env.DB_USERNAME,
